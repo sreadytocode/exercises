365d0fc691c5a04c02d8b4a43bcd1c608aad04a3
sreadytocode <sara.younis2303@gmail.com>
2022-12-09 13:07:29 +0000

    Removed service folder and file

--- client/src/Services/GymService.js
+++
@@ -1,9 +0,0 @@
-import {rapidApiKey} from "../Services/config";
-
-
-      export const fetchData = async (url, options) => {
-        const res = await fetch(url, options);
-        const data = await res.json();
-      
-        return data;
-      };
